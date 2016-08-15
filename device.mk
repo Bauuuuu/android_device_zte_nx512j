c9064a11ffd7fe9667d0127be88cf3a152312f41
Bauuuuu <abansode@outlook.com>
2016-08-15 16:04:03 +0200

    add missing facelock props
    Change-Id: Ib9ec663006581a80987df217d19837eb600d8654

--- device.mk
+++ device.mk
@@ -366,5 +366,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     camera2.portability.force_api=1
 
+# facelock properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.facelock.black_timeout=400 \
+    ro.facelock.det_timeout=1500 \
+    ro.facelock.rec_timeout=2500 \
+    ro.facelock.lively_timeout=2500 \
+    ro.facelock.est_max_time=600 \
+    ro.facelock.use_intro_anim=false
+
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 # $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
