e94c60c5694b24ab39996f33ef71a1e3769a7062
Bauuuuu <abansode@outlook.com>
2016-09-12 10:07:57 +0200

    Cleanup of vibrator  Import CAF audio configs
    * CAF Package version: LA.BR.1.2.9-00810-8x16.0 Import CAF audio configs
    
    Change-Id: I9aee811c5f7f63583631693bc5ec18773025af29

--- device.mk
+++ device.mk
@@ -71,7 +71,17 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
     $(DEVICE_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(DEVICE_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
-    $(DEVICE_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml	
+    $(DEVICE_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml
+
+# XML Audio configuration files
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
+	
 
 # Gello
  PRODUCT_PACKAGES += \
