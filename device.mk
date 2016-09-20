2f1bac74cb1041cbaeb765325e2d243a6a4a79d7
Bauuuuu <abansode@outlook.com>
2016-09-20 15:08:29 +0200

    cleanup for N

--- device.mk
+++ device.mk
@@ -264,14 +264,6 @@ PRODUCT_PACKAGES += \
     Tag \
     com.android.nfc_extras
 
-# KeyHandler
-PRODUCT_PACKAGES += \
-    com.cyanogenmod.keyhandler
-
-PRODUCT_SYSTEM_SERVER_JARS += com.cyanogenmod.keyhandler
-# never dexopt the keyhandler
-$(call add-product-dex-preopt-module-config,com.cyanogenmod.keyhandler,disable)
-
 # Keystore
 PRODUCT_PACKAGES += \
     keystore.msm8916
