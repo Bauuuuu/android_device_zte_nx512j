b3f02991250e28676518355be86b210dcb6e7959
Bauuuuu <abansode@outlook.com>
2016-09-24 11:51:28 +0200

    Add telephony configs
    Change-Id: Iba072f32ab0bd20066c70d1e32555d459f1d0e10

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -16,3 +16,16 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml \
     $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml
 endif
+
+# Telephony
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/telephony/default_static_config.xml:system/etc/telephony/default_static_config.xml \
+    $(LOCAL_PATH)/telephony/N958St_Z0-.xml:system/etc/telephony/N958St_Z0-.xml \
+    $(LOCAL_PATH)/telephony/NX505J_Z0-0.xml:system/etc/telephony/NX505J_Z0-0.xml \
+    $(LOCAL_PATH)/telephony/NX507J_Z0-0.xml:system/etc/telephony/NX507J_Z0-0.xml \
+    $(LOCAL_PATH)/telephony/NX508H-0.xml:system/etc/telephony/NX508H-0.xml \
+    $(LOCAL_PATH)/telephony/NX511J_Z0-1.xml:system/etc/telephony/NX511J_Z0-1.xml \
+    $(LOCAL_PATH)/telephony/NX511J_Z95-7.xml:system/etc/telephony/NX511J_Z95-7.xml \
+    $(LOCAL_PATH)/telephony/NX512J-3.xml:system/etc/telephony/NX512J-3.xml \
+    $(LOCAL_PATH)/telephony/NX512J_Z0-1.xml:system/etc/telephony/NX512J_Z0-1.xml \
+    $(LOCAL_PATH)/telephony/NX512J_Z78-1.xml:system/etc/telephony/NX512J_Z78-1.xml
