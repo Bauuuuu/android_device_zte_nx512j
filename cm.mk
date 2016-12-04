1e5dda20fbec727879b32c0c2189f4a5eb54ad10
Bauuuuu <abansode@outlook.com>
2016-12-04 07:04:33 +0100

    remove NFC enhanced
    Change-Id: Ic74a3c5d255765a472ce6bdf913187880547a95f

--- cm.mk
+++ cm.mk
@@ -14,9 +14,6 @@
 
 $(call inherit-product, device/zte/nx512j/full_nx512j.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
